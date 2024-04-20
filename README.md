30e3fa006670cbbadd26b9510d8166e165cf6934
Eric <ericl16384@gmail.com>
2024-04-20 15:18:43 -0700

    got a bot running around in a square :)))

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # lua-api-learning
 http://lua-users.org/wiki/SimpleLuaApiExample
 
-I'm just learning how to make a secure Lua API in Python.
+I'm just learning how to make a secure Lua API in Python. It sounds fun for programming games.
--- main.py
+++ main.py
@@ -25,16 +25,58 @@ print_log = []
 globals.print = lambda *args: print_log.append(args)
 
 
-# globals.zzz = "seven"
+
+
+
+# a little game
+
+import numpy as np
+import random
+
+pos = (0, 0)
+map = {tuple(pos): "empty"}
+
+def get_tile(xy):
+    xy = tuple(xy)
+    if xy not in map:
+        r = random.random()
+        # if r < 0.1:
+        if max(xy) > 2 or min(xy) < -2:
+            map[xy] = "wall"
+        else:
+            map[xy] = "empty"
+    return map[xy]
+
+def move(new_pos):
+    global pos
+
+    print(pos)
+    input()
+
+    if get_tile(new_pos) == "empty":
+        pos = new_pos
+        return True
+    return False
+
+globals.move_up = lambda: move((pos[0], pos[1] + 1))
+globals.move_left = lambda: move((pos[0] - 1, pos[1]))
+globals.move_down = lambda: move((pos[0], pos[1] - 1))
+globals.move_right = lambda: move((pos[0] + 1, pos[1]))
+
+
+
+
+
+
 
 
 # Load script
 with open("script.lua", "r") as f:
     script = f.read()
-try:
-    lua.execute(script)
-except:
-    print(traceback.format_exc())
+# try:
+lua.execute(script)
+# except:
+#     print(traceback.format_exc())
 
 
 print("caught print output:")
