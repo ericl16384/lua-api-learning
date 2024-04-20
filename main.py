6274370eafb935f904c3bad877cadb83ec3cef64
Eric <ericl16384@gmail.com>
2024-04-20 14:42:53 -0700

    moved environment to main.py and improved print capture

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
 # https://pypi.org/project/lupa/
 import lupa
 
-
-# set up lua security
-
-# attribute_blacklist = ["io"]
-
-# def getter(obj, attr_name):
-#     print(obj, attr_name)
-#     for a in attribute_blacklist:
-#         if attr_name.startswith(a):
-#             raise AttributeError(f"not allowed to read attribute {attr_name}")
-#     return getattr(obj, attr_name)
-# def setter(obj, attr_name, value):
-#     print(obj, attr_name)
-#     for a in attribute_blacklist:
-#         if attr_name.startswith(a):
-#             raise AttributeError(f"not allowed to write attribute {attr_name}")
-#     setattr(obj, attr_name, value)
-
-# def filter_attribute_access(obj, attr_name, is_setting):
-#     # if isinstance(attr_name, unicode):
-#     if not attr_name.startswith("_"):
-#         return attr_name
-#     raise AttributeError("access denied")
-
-
-# class LockedLuaAPI:
-
-
-
 # Initialize Lua runtime
 lua = lupa.LuaRuntime(
     register_eval=False,
@@ -39,10 +10,17 @@ lua = lupa.LuaRuntime(
 )
 globals = lua.globals()
 
-# Load environment (very important to security!!!)
-with open("environment.lua", "r") as f:
-    assert lua.execute(f.read()) == "environment.lua success"
+# Block dangerous functions (very important to security!!!)
+blocked_globals = ["debug", "io", "os", "package", "python"]
+for b in blocked_globals:
+    globals[b] = None
 
+# Capture print statments (otherwise they go to stdout)
+print_log = []
+globals.print = lambda *args: print_log.append(args)
+
+
+# globals.zzz = "seven"
 
 
 # Load script
@@ -52,25 +30,8 @@ lua.execute(script)
 
 
 print("caught print output:")
-for l in globals.__caught_print_output.split("\n"):
-    print(">>>", l)
+# for l in globals.__caught_print_output.split("\n"):
+#     print(">>>", l)
+for l in print_log:
+    print("\t".join(l))
 
-
-
-
-# func = lua.eval("function(f, n) return f(n) end")
-
-# # Expose a Python function to Lua
-# # @lua.register
-# def add(a, b):
-#     return a + b
-
-# # Load and execute Lua script
-# lua.execute("""
-# result = add(10, 20)
-# print(result)
-# """)
-
-# # Accessing Lua variables from Python
-# result = lua.globals().result
-# print("Result from Lua:", result)
