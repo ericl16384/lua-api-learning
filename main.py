83f3ffd07ab3abd5bc776e3f457a60e7429bd510
Eric <ericl16384@gmail.com>
2024-04-20 16:21:26 -0700

    made a little maze, but it's hard to se how to did without, well, SEEING

--- main.py
+++ main.py
@@ -30,38 +30,80 @@ globals.print = lambda *args: print_log.append(args)
 
 # a little game
 
-import numpy as np
+# import numpy as np
 import random
 
+facings = ( # counterclockwise
+    (1, 0),
+    (0, 1),
+    (-1, 0),
+    (0, -1)
+)
+
 pos = (0, 0)
+facing = 0
 map = {tuple(pos): "empty"}
+
+# a little maze
+def clear_area(min_xy, max_xy):
+    for x in range(min_xy[0], max_xy[0] + 1):
+        for y in range(min_xy[1], max_xy[1] + 1):
+            map[(x, y)] = "empty"
+clear_area((0, 0), (4, 0))
+clear_area((4, -2), (4, 3))
 
 def get_tile(xy):
     xy = tuple(xy)
     if xy not in map:
         r = random.random()
         # if r < 0.1:
-        if max(xy) > 2 or min(xy) < -2:
+        if max(xy) > 1 or min(xy) < -1:
             map[xy] = "wall"
         else:
             map[xy] = "empty"
     return map[xy]
 
+def print_state(action):
+    out = ""
+    out += ["east", "north", "west", "south"][facing]
+    out += "\t"
+    out += str(pos)
+    out += "\t--> "
+    out += action
+    input(out)
+
 def move(new_pos):
     global pos
+    new_pos = tuple(new_pos)
 
-    print(pos)
-    input()
+    print_state("move")
 
     if get_tile(new_pos) == "empty":
         pos = new_pos
         return True
     return False
 
-globals.move_up = lambda: move((pos[0], pos[1] + 1))
-globals.move_left = lambda: move((pos[0] - 1, pos[1]))
-globals.move_down = lambda: move((pos[0], pos[1] - 1))
-globals.move_right = lambda: move((pos[0] + 1, pos[1]))
+def turn(amount):
+    assert amount in (1, -1)
+    global facing
+
+    print_state("turn " + {1:"left",-1:"right"}[amount])
+
+    facing += amount
+    facing %= len(facings)
+
+
+# globals.move_up = lambda: move((pos[0], pos[1] + 1))
+# globals.move_left = lambda: move((pos[0] - 1, pos[1]))
+# globals.move_down = lambda: move((pos[0], pos[1] - 1))
+# globals.move_right = lambda: move((pos[0] + 1, pos[1]))
+
+globals.move_forward = lambda: move((
+    pos[0] + facings[facing][0],
+    pos[1] + facings[facing][1]
+))
+globals.turn_right = lambda: turn(-1)
+globals.turn_left = lambda: turn(1)
 
 
 
--- script.lua
+++ script.lua
@@ -1,6 +1,40 @@
-while true do
-  while move_up() do end
-  while move_left() do end
-  while move_down() do end
-  while move_right() do end
+-- while true do
+--   while move_up() do end
+--   while move_left() do end
+--   while move_down() do end
+--   while move_right() do end
+-- end
+
+-- solves mazes without loops
+function amazing()
+  moves = 0
+  while move_forward() do
+    moves = moves + 1
+  end
+
+  -- right path
+  turn_right()
+  if move_forward() then
+    amazing()
+    move_forward()
+  end
+
+  -- left path
+  turn_right()
+  turn_right()
+  if move_forward() then
+    amazing()
+    move_forward()
+  end
+
+  -- back up
+  turn_left()
+  while moves > 0 do
+    move_forward()
+    moves = moves - 1
+  end
+  turn_left()
+  turn_left()
 end
+
+amazing()
